28d4eee7b626755fdbeb93ad4cc90cc1fcea5924
Sridhar3908 <sridhar.manikandan@syncfusion.com>
2020-08-24 15:53:34 +0530

    New10  commit!

--- automate-mr.js
+++ automate-mr.js
@@ -44,26 +44,12 @@ function mergeRequest()
     require('simple-git')()
     .init()
     .add('./*')
-    .commit("New1  commit!")
+    .commit("New10  commit!")
     .addRemote('origin', 'https://github.com/Sridhar2908/javascript-reporting-controls')
-    .exec('git push');
+    .push(['-u', 'origin', 'automate-mr'], () => console.log('done'));
  
-    
-// const simpleGit = require('simple-git');
-// const git = simpleGit(); 
-// try {
-//     const mergeSummary = git.merge();
-//     console.log("Merged");
-//    // console.log(`Merged ${ mergeSummary.merges.length } files`);
-//   }
-//   catch (e) {
-//     // err.message - the string summary of the error
-//     // err.stack - some stack trace detail
-//     // err.git - where a parser was able to run, this is the parsed content
-//    console.log(e);
-//    // console.error(`Merge resulted in ${ err.git.conflicts.length } conflicts`);
-//   }
-//  shelljs.exec('git push');
+ 
+  //shelljs.exec('git push');
 
 
 //   require('simple-git')()
@@ -78,4 +64,22 @@ function mergeRequest()
 //      }
 //   })
 //   .exec(() => console.log('pull done.'));
+
+
+   
+// const simpleGit = require('simple-git');
+// const git = simpleGit(); 
+// try {
+//     const mergeSummary = git.merge();
+//     console.log("Merged");
+//    // console.log(`Merged ${ mergeSummary.merges.length } files`);
+//   }
+//   catch (e) {
+//     // err.message - the string summary of the error
+//     // err.stack - some stack trace detail
+//     // err.git - where a parser was able to run, this is the parsed content
+//    console.log(e);
+//    // console.error(`Merge resulted in ${ err.git.conflicts.length } conflicts`);
+//   }
+
 }
