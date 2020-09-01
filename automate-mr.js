d8f7e9d7ba31a048b0623d939cfa5454841ea76d
Sridhar2908 <sridhar.manikandan@syncfusion.com>
2020-09-01 15:11:24 +0530

    Merge Request 3.10

--- automate-mr.js
+++ automate-mr.js
@@ -34,16 +34,17 @@ async function mergeRequest() {
         .add('./*')
         .commit(`Merge Request 3.10`)
         .addRemote('origin', 'https://github.com/Sridhar2908/javascript-reporting-controls.git');
+        await git().pull((err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
     //.push('origin', 'automate-mr');
 
     await git().push(['-u', 'origin', 'automate-mr'], () => console.log('done'));
     //shellJs.exec(`git push -b ${branch}`)
     //pull 
-    await git().pull((err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
+   
     // Merge 
     git().merge((err) => {
         if (err.git) {
